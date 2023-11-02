8d24b8fd97eb9a2f3e71a33fdef1b14253b95815
nguyenlam1962c5 <nguyenlam1962c5@gmail.com>
2023-11-02 21:41:55 +0700

    first commit

---
+++ Assets/FPS/Scripts/AI/EnemiesSpawn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiesSpawn : MonoBehaviour
+{
+    public GameObject[] enemyPrefabs;
+    public int xSpawn;
+    public int zSpawn;
+    public int maxSpawn;
+    public float timeDelay =3f ;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnEnemies());
+    }
+
+    private IEnumerator SpawnEnemies()
+    {
+        for (int i = 0; i < maxSpawn; i++)
+        {
+
+            Vector3 spawnPosition = new Vector3(xSpawn, 2, zSpawn);
+
+            int randomIndex = Random.Range(0, enemyPrefabs.Length);
+
+
+            Instantiate(enemyPrefabs[randomIndex], spawnPosition, Quaternion.identity);
+
+            yield return new WaitForSeconds(timeDelay);
+        }
+    }
+}
---
+++ Assets/FPS/Scripts/AI/Spawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] enemyPrefabs; // Danh sách các enemy prefab
+    public Transform spawnPoint; // Vị trí spawn cố định
+    public float spawnDelay = 5f; // Thời gian delay giữa các lần spawn
+    public int maxEnemies = 10; // Giới hạn số lượng enemy trên map
+
+    private List<GameObject> spawnedEnemies = new List<GameObject>();
+
+    private void Start()
+    {
+        // Bắt đầu gọi hàm SpawnEnemy sau mỗi khoảng thời gian spawnDelay
+        StartCoroutine(SpawnEnemy());
+    }
+
+    private IEnumerator SpawnEnemy()
+    {
+        while (true)
+        {
+            // Kiểm tra nếu đã đạt đến giới hạn enemy trên map
+            if (spawnedEnemies.Count < maxEnemies)
+            {
+                // Ngẫu nhiên chọn một enemy từ danh sách và spawn tại vị trí cố định
+                GameObject randomEnemyPrefab = enemyPrefabs[Random.Range(0, enemyPrefabs.Length)];
+                GameObject newEnemy = Instantiate(randomEnemyPrefab, spawnPoint.position, Quaternion.identity);
+                spawnedEnemies.Add(newEnemy);
+            }
+
+            yield return new WaitForSeconds(spawnDelay);
+        }
+    }
+
+    // Hàm để xóa enemy khỏi danh sách khi bị hủy
+    public void RemoveEnemy(GameObject enemy)
+    {
+        spawnedEnemies.Remove(enemy);
+    }
+}
